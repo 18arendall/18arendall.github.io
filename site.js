187b3d8c8381b7bfcfe14d9c8862e2fbe41c4262
Adam Rendall <300037469@fvtc.edu>
2026-02-04 14:36:13 -0600

    Added welcome div

--- site.js
+++ site.js
@@ -1,1 +1,25 @@
 console.log("Hi guys")
+
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+const divMessage = document.querySelector('#welcome')
+
+const p = document.createElement('p')
+divMessage.append(p)
+
+if(isMorning == true){
+    
+    p.innerHTML = `<p>Good Morning</p>`
+}
+else if(isAfternoon == true){
+    
+    p.innerHTML = `<p>Good Afternoon</p>`
+}
+else{
+    
+    p.innerHTML = `<p>Good Evening</p>`
+}
